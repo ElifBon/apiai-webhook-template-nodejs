bae9ee691e806d1879050e8601f3e78dd2e47ea9
ElifBon <elifboncuk88@gmail.com>
2017-02-25 22:28:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -33,10 +33,11 @@ app.post('/', function (req, res) {
 
   // Make movie name
   function makeName (assistant) {
-    let movie = assistant.getArgument(MOVIE_ARGUMENT);
+    
     request('http://www.google.com', function (error, response, body) {
       if (!error && response.statusCode == 200) {
           console.log(body) // Print the google web page.
+        let movie = assistant.getArgument(MOVIE_ARGUMENT);
         assistant.tell('Alright, your favourite movie name is ' +
       movie  +
       '! I hope you like it. See you next time.');
